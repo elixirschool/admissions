58cbc5a1ce10afa95fe0754086bc7786b3a24ecc
Sean Callan <seancallan@gmail.com>
2018-12-01 14:17:43 -0700

    Webpack config to reduce bundle size (#8)

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,4 +2,11 @@
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
 import css from "../css/app.scss"
-import '@fortawesome/fontawesome-free/js/all'
+
+import { library, dom } from '@fortawesome/fontawesome-svg-core'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+
+library.add(faEnvelope, faGithub);
+
+dom.watch();
