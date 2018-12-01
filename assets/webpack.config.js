4c5023b275532a8124fb3c7494e1aebcd4132a90
Sean Callan <seancallan@gmail.com>
2018-12-01 07:44:11 -0700

    Webpack config to reduce bundle size

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -6,8 +6,6 @@ const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-const devMode = process.env.NODE_ENV !== 'production'
-
 module.exports = (env, options) => ({
   optimization: {
     minimizer: [
@@ -34,7 +32,7 @@ module.exports = (env, options) => ({
       {
         test: /\.(sa|sc|c)ss$/,
         use: [
-          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          MiniCssExtractPlugin.loader,
           'css-loader',
           'sass-loader'
         ]
